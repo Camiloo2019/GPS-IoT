6e562dc7db9c33cd41bd844b5f4cb06c2a98d9a2
Juanpguti <juanpa0106@hotmail.com>
2023-06-10 10:38:53 -0500

    image

--- GPS.ino
+++ GPS.ino
@@ -1,7 +1,7 @@
-#include <TinyGPS++.h> // library for GPS module
-#include <SoftwareSerial.h>
-#include <WiFi.h>
-TinyGPSPlus gps;  // The TinyGPS++ object
+#include <TinyGPS++.h> // Library for GPS module
+#include <SoftwareSerial.h> //  Allows you to use a regular Arduino serial port as a second serial port
+#include <WiFi.h>  //  Allows you to connect your Arduino to a WiFi network
+TinyGPSPlus gps;  // The TinyGPS++ object (For GPS neo 6m)
 SoftwareSerial ss(21, 22); // The serial connection to the GPS device
 
 const char *ssid =  "*name";     // Enter your WiFi Name
@@ -9,7 +9,7 @@ const char *pass =  "*password"; // Enter your WiFi Password
 float latitude , longitude;
 int year , month , date, hour , minute , second;
 String date_str , time_str , lat_str , lng_str;
-int pm;
+int pm;                         // Variables
 WiFiServer server(80);
 
 void setup()
@@ -19,8 +19,8 @@ void setup()
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, pass); //connecting to wifi
-  while (WiFi.status() != WL_CONNECTED)// while wifi not connected
+  WiFi.begin(ssid, pass); //Connecting to wifi
+  while (WiFi.status() != WL_CONNECTED)// While wifi not connected
   {
     delay(500);
     Serial.print("."); //print "...."
@@ -35,17 +35,17 @@ void setup()
 
 void loop()
 {
-  while (ss.available() > 0) //while data is available
-    if (gps.encode(ss.read())) //read gps data
+  while (ss.available() > 0) //While data is available
+    if (gps.encode(ss.read())) //Read gps data (In this case we used GPS neo 6m)
     {
-      if (gps.location.isValid()) //check whether gps location is valid
+      if (gps.location.isValid()) //Check whether gps location is valid
       {
         latitude = gps.location.lat();
-        lat_str = String(latitude , 6); // latitude location is stored in a string
+        lat_str = String(latitude , 6); // Latitude location is stored in a string
         longitude = gps.location.lng();
-        lng_str = String(longitude , 6); //longitude location is stored in a string
+        lng_str = String(longitude , 6); //Longitude location is stored in a string
       }
-      if (gps.date.isValid()) //check whether gps date is valid
+      if (gps.date.isValid()) //Check whether gps date is valid
       {
         date_str = "";
         date = gps.date.day();
@@ -53,24 +53,24 @@ void loop()
         year = gps.date.year();
         if (date < 10)
           date_str = '0';
-        date_str += String(date);// values of date,month and year are stored in a string
+        date_str += String(date);// Values of date,month and year are stored in a string
         date_str += " / ";
 
         if (month < 10)
           date_str += '0';
-        date_str += String(month); // values of date,month and year are stored in a string
+        date_str += String(month); // Values of date,month and year are stored in a string
         date_str += " / ";
         if (year < 10)
           date_str += '0';
-        date_str += String(year); // values of date,month and year are stored in a string
+        date_str += String(year); // Values of date,month and year are stored in a string
       }
-      if (gps.time.isValid())  //check whether gps time is valid
+      if (gps.time.isValid())  //Check whether gps time is valid
       {
         time_str = "";
         hour = gps.time.hour();
         minute = gps.time.minute();
         second = gps.time.second();
-        minute = (minute + 30); // converting to IST
+        minute = (minute + 30); // Converting to IST
         if (minute > 59)
         {
           minute = minute - 60;
@@ -78,23 +78,23 @@ void loop()
         }
         hour = (hour + 5) ;
         if (hour > 23)
-          hour = hour - 24;   // converting to IST
-        if (hour >= 12)  // checking whether AM or PM
+          hour = hour - 24;   // Converting to IST
+        if (hour >= 12)  // Checking whether AM or PM
           pm = 1;
         else
           pm = 0;
         hour = hour % 12;
         if (hour < 10)
           time_str = '0';
-        time_str += String(hour); //values of hour,minute and time are stored in a string
+        time_str += String(hour); //Values of hour,minute and time are stored in a string
         time_str += " : ";
         if (minute < 10)
           time_str += '0';
-        time_str += String(minute); //values of hour,minute and time are stored in a string
+        time_str += String(minute); //Values of hour,minute and time are stored in a string
         time_str += " : ";
         if (second < 10)
           time_str += '0';
-        time_str += String(second); //values of hour,minute and time are stored in a string
+        time_str += String(second); //Values of hour,minute and time are stored in a string
         if (pm == 1)
           time_str += " PM ";
         else
@@ -107,7 +107,7 @@ void loop()
   {
     return;
   }
-  // Prepare the response
+  // Prepare the response with a little interface 
   String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n <!DOCTYPE html> <html> <head> <title>GPS DATA</title> <style>";
   s += "a:link {background-color: YELLOW;text-decoration: none;}";
   s += "table, th, td </style> </head> <body> <h1  style=";
@@ -129,6 +129,6 @@ void loop()
  
   s += "</body> </html>";
 
-  client.print(s); // all the values are send to the webpage
+  client.print(s); // All the values are send to the webpage
   delay(100);
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GPS-IoT
 # La dorda huele a pis y es una bofe
+
+![ESP32 wemos lolin](C:\Users\Skull\GPS-IoT\Images\ESP32.jpg)  
